cd313dfe7b0fb38788b96b62ffd8bdd63c8bb025
Tanja Mayerhofer <mayerhofer@big.tuwien.ac.at>
2016-08-31 13:51:40 +0200

    Adds dimensions PlaneAngleMeasurement and SolidAngleMeasurement

--- plugins/org.modelexecution.quantitytypes.java/src/org/modelexecution/quantitytypes/java/BaseDimensions.java
+++ plugins/org.modelexecution.quantitytypes.java/src/org/modelexecution/quantitytypes/java/BaseDimensions.java
@@ -1,11 +1,13 @@
 package org.modelexecution.quantitytypes.java;
 
-// NOTE: This file should be in synch with BaseUnitsLiterals.java. 
-// For every Dimension here there should be one Unit in that file, 
-// in the same position in the list of literals
+/*
+ * NOTE: This file should be in synch with BaseUnits.java. 
+ * For every Dimension here there should be one Unit in that file, 
+ * in the same position in the list of literals
+ */
 
 public enum BaseDimensions {
 
-	Length, Mass, Time, ElectricCurrent, ThermodynamicTemperature, AmountOfSubstance, LuminousIntensity
+	Length, Mass, Time, ElectricCurrent, ThermodynamicTemperature, AmountOfSubstance, LuminousIntensity, PlaneAngleMeasurement, SolidAngleMeasurement
 
 }
